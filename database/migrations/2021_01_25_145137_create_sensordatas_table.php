99771ad08a276e427750cdde0af9ff1ab8880035
Seppevanheurck <45237949+Seppevanheurck@users.noreply.github.com>
2021-01-25 15:06:56 +0100

    database aangepast

--- database/migrations/2021_01_25_145137_create_sensordatas_table.php
+++ database/migrations/2021_01_25_145137_create_sensordatas_table.php
@@ -14,13 +14,11 @@ class CreateSensordatasTable extends Migration
     public function up()
     {
         Schema::create('sensordatas', function (Blueprint $table) {
-            $table->bigIncrements('id');
             $table->integer('afstand1');
             $table->integer('afstand2');
             $table->boolean('kade1');
             $table->boolean('kade2');
             $table->timestamp('tijdstip');
-            $table->timestamps();
         });
         DB::table('sensordatas')->insert(
             []);
