3562eab170138ef0515a053ffad409ce4e456418
Christoph Verstappen <45077870+ChristophVerstappen@users.noreply.github.com>
2021-01-25 13:12:34 +0100

    Implementatie DataTables

--- resources/views/admin/bedrijven/bedrijven.blade.php
+++ resources/views/admin/bedrijven/bedrijven.blade.php
@@ -13,7 +13,7 @@
 
     <div class="row">
         <div class="table-responsive col-12">
-            <table class="table">
+            <table class="table" id="bedrijventable">
                 <thead>
                 <tr>
                     <th>Bedrijf</th>
@@ -33,6 +33,33 @@
         @endsection
         @section('script_after')
             <script>
+                $(document).ready(function () {
+                    $('#bedrijventable').DataTable({
+                        language: {
+                            "sProcessing": "Bezig...",
+                            "sLengthMenu": "_MENU_ resultaten weergeven",
+                            "sZeroRecords": "Geen resultaten gevonden",
+                            "sInfo": "_START_ tot _END_ van _TOTAL_ resultaten",
+                            "sInfoEmpty": "Geen resultaten om weer te geven",
+                            "sInfoFiltered": " (gefilterd uit _MAX_ resultaten)",
+                            "sInfoPostFix": "",
+                            "sSearch": "Zoeken:",
+                            "sEmptyTable": "Geen resultaten aanwezig in de tabel",
+                            "sInfoThousands": ".",
+                            "sLoadingRecords": "Een moment geduld aub - bezig met laden...",
+                            "oPaginate": {
+                                "sFirst": "Eerste",
+                                "sLast": "Laatste",
+                                "sNext": "Volgende",
+                                "sPrevious": "Vorige"
+                            },
+                            "oAria": {
+                                "sSortAscending": ": activeer om kolom oplopend te sorteren",
+                                "sSortDescending": ": activeer om kolom aflopend te sorteren"
+                            }
+                        }
+                    });
+                });
                 $(function () {
                     loadTable();
 
