4dd7306c912859a4bf4a884ab0aa2457e306af4d
Seppevanheurck <45237949+Seppevanheurck@users.noreply.github.com>
2021-02-16 11:26:08 +0100

    return to aangepast'

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -70,7 +70,7 @@
                 //check if user is authenticated; if not, redirect to login page
                 if(response.status===401){
                     const url = new URL(`${urlQlikServer}login`);
-                    url.searchParams.append('returnto', 'https://localhost:3000');
+                    url.searchParams.append('returnto', 'https://projectd2.westeurope.cloudapp.azure.com');
                     url.searchParams.append('qlik-web-integration-id', webIntegrationId);
 
                     window.location.href = url;
